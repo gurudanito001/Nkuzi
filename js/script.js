f9c671e8180455c90df490d621a85a07f0357117
Daniel Nwokocha <gurudanito001@gmail.com>
2020-06-23 13:05:39 +0100

    Commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+function scrollLeft(){
+    document.querySelector('.scrolling-wrapper').scrollLeft += 285;
+}
+function scrollRight(){
+    document.querySelector('.scrolling-wrapper').scrollLeft -= 285;
+}
+(function listen(){
+    let chevronRight = document.querySelector('.chevron-right');
+    chevronRight.addEventListener('click', scrollLeft);
+    let chevronLeft = document.querySelector('.chevron-left');
+    chevronLeft.addEventListener('click', scrollRight);
+})()
+
+
